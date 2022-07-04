7a35950aab2fea0eb23181f0b51077c1ed64e231
Oriol Angelet <uriaeri@gmail.com>
2022-07-04 18:17:04 +0200

    Creation of buttons Mines, Time & Reset Title First iteration of grid number of items & cellforitemat

---
+++ Minesweeper/SquareCell.swift
@@ -0,0 +1,12 @@
+//
+//  Square.swift
+//  Minesweeper
+//
+//  Created by Uri on 4/7/22.
+//
+
+import UIKit
+
+class SquareCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
--- Minesweeper/ViewController.swift
+++ Minesweeper/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    
+    private lazy var rightBarButtonItem: UIBarButtonItem = {
+        UIBarButtonItem(title: "Mines left: 0", style: .plain, target: self, action: nil)
+    }()
+    private lazy var leftBarButtonItem: UIBarButtonItem = {
+        UIBarButtonItem(title: "Time: 0", style: .plain, target: self, action: nil)
+    }()
+    
+    private var mines = 0 {
+        didSet {
+            rightBarButtonItem.title = "Mines left: \(mines)"
+        }
+    }
+    private var time = 0 {
+        didSet {
+            leftBarButtonItem.title = "Time: \(time)"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavigation()
+        
     }
-
+    
+    // how many squares our board has
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 16
+    }
+    
+    // how our squares look
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Square", for: indexPath) as? SquareCell else { fatalError("Unable to dequeue SquareCell")
+        }
+        return cell
+    }
+    
+    
+    private func configureNavigation() {    // UI of our app
+        title = "Minesweeper"
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        let resetButton = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetGame))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) // used to center resetButton
+        toolbarItems = [spacer, resetButton, spacer]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    @objc func resetGame() { // method to reset the game
+        
+    }
 
 }
 
